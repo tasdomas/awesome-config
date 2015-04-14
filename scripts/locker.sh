7b7197fb895e8e1651156d2f6f1e41f846789cbf
Domas Monkus <domas@monkus.lt>
2015-04-14 20:08:04 +0200

    Use lightdm to lock screen.

--- scripts/locker.sh
+++ scripts/locker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+exec run-one light-locker --lock-on-suspend&
+
 exec xautolock -detectsleep \
-  -time 3 -locker "slock" \
+  -time 3 -locker "light-locker-command -l" \
   -notify 30 \
   -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
